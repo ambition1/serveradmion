f1fca8cdffc1493607fa83932f5acd402db29784
yang <1791784037@qq.com>
2019-01-14 22:18:52 +0800

    Initial commit

---
+++ src/main/java/com/yang/adminserver/AdminserverApplication.java
@@ -0,0 +1,23 @@
+package com.yang.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminserverApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminserverApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdminserverApplication.class);
+    }
+
+}
+
